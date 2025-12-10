23e2f5281887d0f5da8298be99b6d053dd6c4840
ntorh <ngcobontombenhle434@gmail.com>
2025-12-10 17:41:30 +0200

    feat: secure TabLayout with ProtectedRoute and add tab icons for all sections

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,57 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
+import ProtectedRoute from "../../components/ProtectedRoute";
+
+export default function TabLayout() {
+  return (
+    <ProtectedRoute>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#007AFF",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: {
+            backgroundColor: "white",
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "All Notes",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="work"
+          options={{
+            title: "Work",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="briefcase" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="study"
+          options={{
+            title: "Study",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="book" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="personal"
+          options={{
+            title: "Personal",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </ProtectedRoute>
+  );
+}
